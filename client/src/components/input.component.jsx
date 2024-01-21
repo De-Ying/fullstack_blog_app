d748529cdc569e56df7001120a36e9377dd01d9b
De-Ying-Course <ducanh25820@gmail.com>
2024-01-21 23:21:13 +0700

    User Auth Form UI

---
+++ client/src/components/input.component.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const InputBox = ({ name, type, id, value, placeholder, icon}) => {
+
+    const [ passwordVisible, setPasswordVisible ] = useState(false);
+
+    return (
+        <div className="relative w-[100%] mb-4">
+            <input 
+                name={name}
+                type={type == "password" ? (passwordVisible ? "text" : "password") : type}
+                placeholder={placeholder}
+                defaultValue={value}
+                id={id}
+                className="input-box"
+            />
+
+            <i className={"fi " + icon + " input-icon"}></i>
+
+            {
+                type == "password" ?
+                    <i 
+                        className={"fi fi-rr-eye" + (!passwordVisible ? "-crossed" : "")  + " input-icon left-[auto] right-4 cursor-pointer"}
+                        onClick={() => setPasswordVisible(currentVal => !currentVal)}
+                    ></i>
+                : ""
+            }
+        </div>
+    )
+}
+
+InputBox.propTypes = { 
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+}
+
+export default InputBox;
