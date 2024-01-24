664b1f42a902409258da0eab16494d9b854a86c3
De-Ying-Course <ducanh25820@gmail.com>
2024-01-24 21:26:21 +0700

    Setting up editor routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
+import Editor from "./pages/editor.pages";
 import UserAuthForm from "./pages/userAuthForm.page";
 import UserContextProvider from "./context/user.context";
 
@@ -7,9 +8,10 @@ const App = () => {
   return (
     <UserContextProvider>
       <Routes>
+        <Route path="/editor" element={<Editor />}/>
         <Route path="/" element={<Navbar />}>
-          <Route path="signin" element={<UserAuthForm type="sign-in" />}></Route>
-          <Route path="signup" element={<UserAuthForm type="sign-up" />}></Route>
+          <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+          <Route path="signup" element={<UserAuthForm type="sign-up" />} />
         </Route>
       </Routes>
     </UserContextProvider>
---
+++ client/src/pages/editor.pages.jsx
@@ -0,0 +1,5 @@
+const Editor = () => {
+    return <h1>This is a Editor page</h1>
+}
+
+export default Editor;
