7f439a780bde47448bdb0a6f8d94d8f542b4d917
De-Ying-Course <ducanh25820@gmail.com>
2024-01-24 22:26:23 +0700

    Blog Editor - Banner UI

--- client/src/components/blog-editor.component.jsx
+++ client/src/components/blog-editor.component.jsx
@@ -1,29 +1,56 @@
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
+import AnimationWrapper from "../common/page-animation";
+import defaultBanner from "../imgs/blog banner.png";
 
 const BlogEditor = () => {
-    return (
-        <>
-            <nav className="navbar">
-                <Link to="/" className="flex-none w-10">
-                    <img src={logo} className="w-full" />
-                </Link>
 
-                <p className="max-md:hidden text-black line-clamp-1 w-full">
-                    New Blog
-                </p>
+  const handleBannerUpload = (e) => {
+    let img = e.target.files[0];
+    console.log(img);
+  };
 
-                <div className="flex gap-4 ml-auto">
-                    <button className="btn-dark py-2">
-                        Publish
-                    </button>
-                    <button className="btn-light py-2">
-                        Save Draft
-                    </button>
-                </div>
-            </nav>
-        </>
-    )
-}
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="flex-none w-10">
+          <img src={logo} className="w-full" />
+        </Link>
+
+        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-dark py-2">Publish</button>
+          <button className="btn-light py-2">Save Draft</button>
+        </div>
+      </nav>
+
+      <AnimationWrapper
+        keyValue="blog-editor"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className=""
+      >
+        <section>
+          <div className="mx-auto max-w-[900px] w-full">
+            <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+              <label htmlFor="uploadBanner">
+                <img src={defaultBanner} className="z-20" />
+                <input
+                  id="uploadBanner"
+                  type="file"
+                  accept=".png, .jpg, .jpeg"
+                  hidden
+                  onChange={handleBannerUpload}
+                />
+              </label>
+            </div>
+          </div>
+        </section>
+      </AnimationWrapper>
+    </>
+  );
+};
 
 export default BlogEditor;
