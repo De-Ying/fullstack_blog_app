6a65da4fd5c34cdb839355da593adcee9b024ad2
De-Ying-Course <ducanh25820@gmail.com>
2024-01-31 23:53:36 +0700

    Fetching latest blog

--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
+import Loader from "../components/loader.component";
+import axios from "axios";
 
 const HomePage = () => {
+  const [ blogs, setBlog ] = useState(null);
+
+  const fetchLatestBlogs = async () => {
+    try {
+      const { data } = await axios(
+        import.meta.env.VITE_SERVER_API + "/blog/latest-blog"
+      );
+      
+      setBlog(data.blogs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLatestBlogs();
+  }, []);
+
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10">
@@ -11,7 +32,14 @@ const HomePage = () => {
             routes={["home", "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
-            <h1>Latest Blogs Here</h1>
+            <>
+                {
+                    blogs === null ? <Loader /> :
+                    blogs.map((blog, i) => {
+                        return <h1 key={i}>{blog.title}</h1>
+                    })
+                }
+            </>
 
             <h1>Trending Blogs Here</h1>
           </InPageNavigation>
