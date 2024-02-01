26dd40b8516871ccf9b3726332d90d708fe0ca5e
De-Ying-Course <ducanh25820@gmail.com>
2024-02-01 12:11:57 +0700

    Blog Card UI

--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
+import BlogPostCard from "../components/blog-post.component";
 import Loader from "../components/loader.component";
 import axios from "axios";
 
 const HomePage = () => {
-  const [ blogs, setBlog ] = useState(null);
+  const [blogs, setBlog] = useState(null);
 
   const fetchLatestBlogs = async () => {
     try {
       const { data } = await axios(
         import.meta.env.VITE_SERVER_API + "/blog/latest-blog"
       );
-      
+
       setBlog(data.blogs);
     } catch (error) {
       console.log(error);
@@ -33,12 +34,24 @@ const HomePage = () => {
             defaultHidden={["trending blogs"]}
           >
             <>
-                {
-                    blogs === null ? <Loader /> :
-                    blogs.map((blog, i) => {
-                        return <h1 key={i}>{blog.title}</h1>
-                    })
-                }
+              {blogs === null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      key={i}
+                      className={"mb-7"}
+                    >
+                      <BlogPostCard
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
+                    </AnimationWrapper>
+                  );
+                })
+              )}
             </>
 
             <h1>Trending Blogs Here</h1>
